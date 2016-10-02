d9d0525c2956ea647b446c63526b928b463c92ac
Simon Tyrrell <tyrrell.simon@gmail.com>
2016-10-02 22:48:29 +0100

    wip

--- rest.c
+++ rest.c
@@ -5,13 +5,27 @@
  *      Author: billy
  */
 
+#include <stdlib.h>
+
 #include "rest.h"
 #include "config.h"
 
-static const char * const API_ACTIONS_SS [] =
+
+
+
+typedef struct APICall
 {
-	"metadata",
-	NULL
+	const char * const ac_action_s;
+	int (*ac_callback_fn) (request_rec *req_p, const char *relative_uri_s)
+} APICall;
+
+
+static int SearchMetadata (request_rec *req_p)
+
+static APICall S_API_ACTIONS_P [] =
+{
+		{ "metadata", SearchMetadata },
+		NULL
 };
 
 int DavrodsRestHandler (request_rec *req_p)
@@ -29,11 +43,36 @@ int DavrodsRestHandler (request_rec *req_p)
   				davrods_dir_conf_t *config_p = ap_get_module_config (req_p -> per_dir_config, &davrods_module);
 
   				/*
-  				 * Parse the  uri from req_p -> uri to get the API call
+  				 * Parse the uri from req_p -> uri to get the API call
   				 */
+  				APICall *call_p = S_API_ACTIONS_P;
+
+  				while (call_p != NULL)
+  					{
+  						size_t l = strlen (call_p -> ac_action_s);
+
+  						if ((strncmp (req_p -> uri, call_p -> ac_action_s, l) == 0) && ((* (req_p -> uri) + l) == '/'))
+  							{
+  								res = call_p -> ac_callback_fn (req_p, (reg_p -> uri) + (l + 1));
+
+  								/* force exit from loop */
+  								call_p = NULL;
+  							}
+  					}
 
   			}
   	}
 
   return res;
+}
+
+
+
+static int SearchMetadata (request_rec *req_p, const char *relative_uri_s)
+{
+	int res = DECLINED;
+
+
+
+	return res;
 }
